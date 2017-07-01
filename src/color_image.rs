1f92b66bd5fc7498e69b378ef6bd0ca052dcc12e
hjbyt <sopu00@gmail.com>
2017-07-01 23:09:08 +0300

    added ColorImage

---
+++ src/color_image.rs
@@ -0,0 +1,43 @@
+use std::ops::{Index, IndexMut};
+use color::{Color, BLACK};
+use image::{ImageBuffer, Rgb};
+
+pub struct ColorImage {
+    pub pixels: Vec<Color>,
+    pub height: u32,
+    pub width: u32,
+}
+
+impl ColorImage {
+    pub fn new(width: u32, height: u32) -> ColorImage {
+        let size = (height * width) as usize;
+        ColorImage {
+            pixels: vec![BLACK; size],
+            height: height,
+            width: width,
+        }
+    }
+
+    pub fn to_image_buffer(self) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+        ImageBuffer::<Rgb<u8>, Vec<u8>>::from_fn(self.width, self.height, |x, y| {
+            let (r, g, b) = self[(x as usize, y as usize)].bytes();
+            Rgb([r, g, b])
+        })
+    }
+}
+
+impl Index<(usize, usize)> for ColorImage {
+    type Output = Color;
+
+    fn index(&self, index: (usize, usize)) -> &Color {
+        let (x, y) = index;
+        &self.pixels[(y * self.width as usize + x)]
+    }
+}
+
+impl IndexMut<(usize, usize)> for ColorImage {
+    fn index_mut<'a>(&'a mut self, index: (usize, usize)) -> &'a mut Color {
+        let (x, y) = index;
+        &mut self.pixels[(y * self.width as usize + x)]
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@ mod ray;
 mod hit;
 mod model_object;
 mod camera;
+mod color_image;
+
+extern crate image;
 
 fn main() {
+    let mut img = color_image::ColorImage::new(500, 500);
+    img[(100, 100)] = color::Color::new(1.0, 1.0, 1.0);
+    let img_buf = img.to_image_buffer();
+    img_buf.save("output.png").unwrap();
     println!("{:?}", 123);
 }
