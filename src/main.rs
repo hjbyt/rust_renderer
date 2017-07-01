843eb13cf8bd2d5e90d83b6468f55fc3a866e3c9
hjbyt <sopu00@gmail.com>
2017-07-01 14:21:39 +0300

    added Ray

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod vector;
 mod sphere;
+mod color;
+mod material;
+mod ray;
 
 fn main() {
     let s = sphere::Sphere { center: vector::ZERO, radius: 1.0 };
---
+++ src/ray.rs
@@ -0,0 +1,21 @@
+use vector::Vector;
+
+pub struct Ray {
+    pub position: Vector,
+    pub direction: Vector,
+}
+
+impl Ray {
+    pub fn new(position: Vector, direction: Vector) -> Ray {
+        debug_assert!(direction.norm() == 1.0);
+        Ray { position: position, direction: direction }
+    }
+
+    pub fn construct_ray(from: Vector, to: Vector) -> Ray {
+        Ray::new(from, from.direction_to(to))
+    }
+
+    pub fn advance(&mut self, by: f64) {
+        self.position += self.direction * by;
+    }
+}
