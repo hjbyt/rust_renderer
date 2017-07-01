e4dc4aa2c4d739dfcc1ad61d14e0ddd99d74f2a6
hjbyt <sopu00@gmail.com>
2017-07-01 14:14:10 +0300

    implemented Material

---
+++ src/material.rs
@@ -0,0 +1,27 @@
+use color::{Color, BLACK};
+
+#[derive(Debug, Copy, Clone, Default, PartialEq)]
+pub struct Material {
+    pub diffuse_color: Color,
+    pub specular_color: Color,
+    pub reflection_color: Color,
+    pub phong_specularity: f64,
+    pub transparency: f64,
+}
+
+impl Material {
+    pub fn new(diffuse_color: Color,
+               specular_color: Color,
+               reflection_color: Color,
+               phong_specularity: f64,
+               transparency: f64) -> Material {
+        debug_assert!(0.0 <= transparency && transparency <= 1.0);
+        Material {
+            diffuse_color: diffuse_color,
+            specular_color: specular_color,
+            reflection_color: reflection_color,
+            phong_specularity: phong_specularity,
+            transparency: transparency
+        }
+    }
+}
