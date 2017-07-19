007c17c3c080a3bd6a197b672dd2a7b44e6b2b04
hjbyt <sopu00@gmail.com>
2017-07-19 03:53:48 +0300

    moved changed Light::get_intensity_for_hit into Scene::get_light_intensity_for_hit and completed it

--- src/light.rs
+++ src/light.rs
@@ -1,9 +1,5 @@
 use color::Color;
 use vector::Vector;
-use hit::Hit;
-use rand::{self, Rng};
-use ray::Ray;
-
 
 pub struct Light {
     pub position: Vector,
@@ -26,51 +22,6 @@ impl Light {
             specular_intensity,
             shadow_intensity,
             radius
-        }
-    }
-
-    pub fn get_intensity_for_hit(&self, hit: &Hit) -> f64 {
-        let light_directon = hit.hit_point.direction_to(self.position);
-        let direction_x = if light_directon.x == 0.0 && light_directon.y == 0.0 {
-            Vector::new(1.0, 0.0, 0.0)
-        } else {
-            // Rotate light direction by 90 degrees around z-axis to get an orthogonal vector
-            Vector::new(-light_directon.y, light_directon.x, light_directon.z)
-        };
-        let direction_y = *(light_directon ^ direction_x).normalize();
-
-        //TODO: get from scene
-        let n: u32 = 1;
-        let cell_radius = self.radius / n as f64;
-        let hald_radius = cell_radius / 2.0;
-
-        let mut total_intensity = 0.0;
-        let mut rng = rand::thread_rng();
-        for x in 0..n {
-            for y in 0..n {
-                let x_offset = cell_radius * (x as f64 + rng.next_f64()) - hald_radius;
-                let y_offset = cell_radius * (y as f64 + rng.next_f64()) - hald_radius;
-                let x_delta = direction_x * x_offset;
-                let y_delta = direction_y * y_offset;
-                let cell_point = self.position + x_delta + y_delta;
-                let ray = Ray::construct_ray(cell_point, hit.hit_point);
-                total_intensity += get_ray_intensity(hit, &ray);
-            }
-        }
-
-        let intensity = total_intensity / (n * n) as f64;
-        // Interpolate intensity such that the minimum is shadow_intensity
-        let intensity = 1.0 - ((1.0 - intensity) * self.shadow_intensity);
-        return intensity;
-
-        fn get_ray_intensity(hit: &Hit, ray: &Ray) -> f64 {
-            let mut ray_intensity = 1.0;
-            let max_hit_distance = hit.hit_point.distance_to(ray.position) + ::utils::EPSILON;
-            //for ray_hit :
-            //TODO: continue
-
-
-            ray_intensity
         }
     }
 }
--- src/scene.rs
+++ src/scene.rs
@@ -8,6 +8,8 @@ use color_image::ColorImage;
 use ray::Ray;
 use hit::Hit;
 use light::Light;
+use rand::{self, Rng};
+use utils::almost_eq;
 
 pub struct Scene {
     pub background_color: Color,
@@ -102,6 +104,65 @@ impl Scene {
         }
 
         total_color + self.background_color * prev_transparency
+    }
+
+    pub fn get_light_intensity_for_hit(&self, light: &Light, hit: &Hit) -> f64 {
+        let light_direction = hit.hit_point.direction_to(light.position);
+        let direction_x = if light_direction.x == 0.0 && light_direction.y == 0.0 {
+            Vector::new(1.0, 0.0, 0.0)
+        } else {
+            // Rotate light direction by 90 degrees around z-axis to get an orthogonal vector
+            Vector::new(-light_direction.y, light_direction.x, light_direction.z)
+        };
+        let direction_y = *(light_direction ^ direction_x).normalize();
+
+        //TODO: get from scene
+        let n: u32 = 1;
+        let cell_radius = light.radius / n as f64;
+        let hald_radius = cell_radius / 2.0;
+
+        let mut total_intensity = 0.0;
+        let mut rng = rand::thread_rng();
+        for x in 0..n {
+            for y in 0..n {
+                let x_offset = cell_radius * (x as f64 + rng.next_f64()) - hald_radius;
+                let y_offset = cell_radius * (y as f64 + rng.next_f64()) - hald_radius;
+                let x_delta = direction_x * x_offset;
+                let y_delta = direction_y * y_offset;
+                let cell_point = light.position + x_delta + y_delta;
+                let ray = Ray::construct_ray(cell_point, hit.hit_point);
+                total_intensity += self.get_ray_intensity(hit, &ray);
+            }
+        }
+
+        let intensity = total_intensity / (n * n) as f64;
+        // Interpolate intensity such that the minimum is shadow_intensity
+        let intensity = 1.0 - ((1.0 - intensity) * light.shadow_intensity);
+        return intensity;
+    }
+
+    fn get_ray_intensity(&self, hit: &Hit, ray: &Ray) -> f64 {
+        let mut ray_intensity = 1.0;
+        let max_hit_distance = hit.hit_point.distance_to(ray.position) + ::utils::EPSILON;
+        for ray_hit in self.find_hits(ray) {
+            //TODO: make sure this short circuits
+            // Check if we got to the given hit or if we passed it
+            // (at object edges ray_hit can miss the original ray hit)
+            let objects_equal = ray_hit.object as *const _ == hit.object as * const _;
+            let hits_almost_equal = objects_equal && ray_hit.hit_point.almost_equal_to(hit.hit_point, ::utils::EPSILON);
+            let passed_max_distance = ray_hit.hit_point.distance_to(ray.position) > max_hit_distance;
+            if (hits_almost_equal || passed_max_distance) {
+                break;
+            }
+            // Check if we hit an opaque object
+            if ray_hit.object.material().transparency == 0.0 {
+                ray_intensity = 0.0;
+                break;
+            }
+            // Attenuate intensity
+            ray_intensity *= ray_hit.object.material().transparency;
+        }
+        ray_intensity
     }
 }
 
