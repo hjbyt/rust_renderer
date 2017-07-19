e5b08f6f164a9c5c76ddc88c38b06fa7b469f043
hjbyt <sopu00@gmail.com>
2017-07-19 03:17:08 +0300

    renamed ray_tracer module to scene

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ mod hit;
 mod model_object;
 mod camera;
 mod color_image;
-mod ray_tracer;
+mod scene;
 mod light;
 
 extern crate image;
 extern crate rand;
 
 fn main() {
-    let scene = ray_tracer::get_simple_scene();
+    let scene = scene::get_simple_scene();
     let color_image = scene.render();
     let image_buffer = color_image.to_image_buffer();
     image_buffer.save("output.png").unwrap();
